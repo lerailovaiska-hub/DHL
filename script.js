06ea93face23a3eed77e7172aa735a2bdcd9e480
jaena6 <jaenasofiad@gmail.com>
2026-06-12 17:15:28 +0200

    connect js to loginpage

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('javascript is working');
+// DOM Elements
+const signupForm = document.getElementById('signup-form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirm-password');
+const SignupButton = document.getElementById('signup-button')
+
+const emailError = document.getElementById('email-error');
+const passwordStrength = document.getElementById('passwordStrength');
+const confirmPasswordError = document.getElementById('confirm-password-error');
+const passwordMatchError = document.getElementById('password-match-error');
